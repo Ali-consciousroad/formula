8962358a2ef90ecfb54447addfea4c0e5f44b46f
Ali-consciousroad <ali_dindar@live.be>
2023-04-28 09:27:38 +0200

    Remove unused import

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import InputFormula from "./component/InputFormula";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <div className="App">
-        <InputFormula formula="1+3" />
+        <InputFormula formula="3+4" />
       </div>
     </div>
   );
--- src/component/InputFormula.js
+++ src/component/InputFormula.js
@@ -2,19 +2,27 @@ import React, { useState, useEffect } from 'react';
 import { Parser } from 'hot-formula-parser';
 
 const InputFormula = ({ formula }) => {
+  // Declare a state variable to hold the result of the formula
   const [valueFromFormula, setValueFromFormula] = useState('');
 
+  // Use the useEffect hook to evaluate the formula whenever it changes
   useEffect(() => {
+    // Create a new instance of the Parser
     const parser = new Parser();
+
+    // Parse and evaluate the formula
     const result = parser.parse(formula);
-    
+
+    // If there's an error (e.g., invalid formula), set valueFromFormula to the original formula
     if (result.error) {
       setValueFromFormula(formula);
     } else {
+      // Otherwise, set valueFromFormula to the result of the evaluated formula
       setValueFromFormula(result.result);
     }
   }, [formula]);
 
+  // Render the component, displaying the formula and its result
   return (
     <div>
       <p>Formula: {formula}</p>
